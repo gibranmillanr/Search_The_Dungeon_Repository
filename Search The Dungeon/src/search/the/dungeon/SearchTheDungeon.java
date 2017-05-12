cbfc1657ed791c31f122564bee1e07e13201df6e
Gibran Millan <Gibran Millan@10.4.203.71>
2017-05-11 18:35:13 -0700

    May 11 Creation of player class

---
+++ Search The Dungeon/src/search/the/dungeon/SearchTheDungeon.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package search.the.dungeon;
+
+import byui.cit260.searchTheDungeon.model.Player;
+
+/**
+ *
+ * @author Gibran Millan
+ */
+public class SearchTheDungeon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //player instance
+        Player playerOne = new Player();
+        
+        playerOne.setName("Fred");
+        playerOne.setPowerLevel(20);
+    }
+    
+}
