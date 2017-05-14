906219b31d9dcee7a614a2d3ac3fa8e481bea4fa
pauldarr <pauldarr@gmail.com>
2017-05-13 21:10:13 -0500

    Added scene class.

---
+++ Search The Dungeon/src/byui/cit260/searchTheDungeon/model/Scene.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.searchTheDungeon.model;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Paul Darr
+ */
+public class Scene implements Serializable {
+    
+    // Class instance Variables
+    private String description;
+    private String row;
+    private String column;
+    private String visited;
+    
+    //Default Constructor
+    public Scene() {
+    }
+    
+    //Getter and Setter
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+    public void setRow(String row) {
+        this.row = row;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public String getVisited() {
+        return visited;
+    }
+
+    public void setVisited(String visited) {
+        this.visited = visited;
+    }
+
+    //hashcode
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.description);
+        hash = 29 * hash + Objects.hashCode(this.row);
+        hash = 29 * hash + Objects.hashCode(this.column);
+        hash = 29 * hash + Objects.hashCode(this.visited);
+        return hash;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Scene{" + "description=" + description + ", row=" + row + ", column=" + column + ", visited=" + visited + '}';
+    }
+
+    //equals
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Scene other = (Scene) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.row, other.row)) {
+            return false;
+        }
+        if (!Objects.equals(this.column, other.column)) {
+            return false;
+        }
+        if (!Objects.equals(this.visited, other.visited)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
