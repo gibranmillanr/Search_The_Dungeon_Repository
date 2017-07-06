41bf7bdb9b99625666c3eafe5b14c335dcce213c
pauldarr <pauldarr@gamingbox>
2017-07-05 21:25:19 -0500

    Group assignment Page 10.

--- Search The Dungeon/src/search/the/dungeon/SearchTheDungeon.java
+++ Search The Dungeon/src/search/the/dungeon/SearchTheDungeon.java
@@ -16,6 +16,10 @@ package search.the.dungeon;
 import byui.cit260.searchTheDungeon.model.Game;
 import byui.cit260.searchTheDungeon.model.Player;
 import byui.cit260.searchTheDungeon.view.StartProgramView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 /**
  *
@@ -28,6 +32,36 @@ public class SearchTheDungeon {
      */
     private static Game currentGame = null;
     public static Player player = null;
+    
+    private static PrintWriter outFile = null;
+    private static BufferedReader inFile = null;
+    
+    private static PrintWriter logFile = null;
+    
+    //Getter and Setter
+    public static PrintWriter getOutFile() {
+        return outFile;
+    }
+
+    public static void setOutFile(PrintWriter outFile) {
+        SearchTheDungeon.outFile = outFile;
+    }
+
+    public static BufferedReader getInFile() {
+        return inFile;
+    }
+
+    public static void setInFile(BufferedReader inFile) {
+        SearchTheDungeon.inFile = inFile;
+    }
+    
+    public static PrintWriter getLogFile() {
+        return logFile;
+    }
+
+    public static void setLogFile(PrintWriter logFile) {
+        SearchTheDungeon.logFile = logFile;
+    }
 
     public static Game getCurrentGame() {
         return currentGame;
@@ -45,15 +79,47 @@ public class SearchTheDungeon {
         SearchTheDungeon.player = player;
     }
     public static void main(String[] args) {
-        //create StartProgramViewOrig and display the start program view
-        StartProgramView startProgramView = new StartProgramView();
+        
         try{
-        startProgramView.display();
+            //open character stream files for end user input and output
+            SearchTheDungeon.inFile =
+                    new BufferedReader(new InputStreamReader(System.in));
+            SearchTheDungeon.outFile = new PrintWriter(System.out, true);
+                       
+            //create StartProgramViewOrig and display the start program view
+            StartProgramView startProgramView = new StartProgramView();
+            startProgramView.display();
+        
         } catch (Throwable te) {
             System.out.println(te.getMessage());
-            te.printStackTrace();
-            startProgramView.display();
-          }
+        
+        try {    
+            //open log file
+            String filePath = "log.txt";
+            SearchTheDungeon.logFile = new PrintWriter(filePath);                        
+        
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.toString() +
+                    "\nCause: " + e.getCause() +
+                    "\nMessage: " + e.getMessage());
+        }
+            
+        }
+        
+        finally {
+            try {
+                if (SearchTheDungeon.inFile != null)
+                    SearchTheDungeon.inFile.close();
+                if (SearchTheDungeon.outFile != null)
+                    SearchTheDungeon.outFile.close();
+                if (SearchTheDungeon.logFile != null)
+                    SearchTheDungeon.logFile.close();
+            } catch (IOException ex) {
+                System.out.println("Error closing files");
+                return;
+            }
+            
+        }
     }
 }
     
